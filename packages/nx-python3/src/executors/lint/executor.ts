e6abcd24463e608995c1f57b4f82dadd6abf4861
Dennis James Stelmach <dj@dstelmach.com>
2023-04-03 11:46:09 -0400

    Migrate to latest.

--- packages/nx-python3/src/executors/lint/executor.ts
+++ packages/nx-python3/src/executors/lint/executor.ts
@@ -2,8 +2,6 @@
 
 import { ExecutorContext } from '@nrwl/devkit';
 import { runPipenvCommand } from '../../utils';
-// There's a fake eslint error here
-// eslint-disable-next-line import/extensions
 import { LintExecutorSchema } from './schema';
 
 export default async function runExecutor(
--- packages/nx-python3/src/executors/test/executor.ts
+++ packages/nx-python3/src/executors/test/executor.ts
@@ -2,8 +2,6 @@
 
 import { ExecutorContext } from '@nrwl/devkit';
 import { runPipenvCommand } from '../../utils';
-// There's a fake eslint error here
-// eslint-disable-next-line import/extensions
 import { TestExecutorSchema } from './schema';
 
 export default async function runExecutor(
