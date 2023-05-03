57b4e2205793c6e9fb71a21482066807e8e94a6b
Dennis James Stelmach <dj@dstelmach.com>
2023-05-03 00:31:40 -0400

    Formatting.

--- jest.config.app.ts
+++ jest.config.app.ts
@@ -1,13 +1,13 @@
 /* eslint-disable */
 export default {
-  displayName: "nx-pipenv",
-  preset: "./jest.preset.js",
+  displayName: 'nx-pipenv',
+  preset: './jest.preset.js',
   transform: {
-    "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  moduleFileExtensions: ["ts", "js", "html"],
+  moduleFileExtensions: ['ts', 'js', 'html'],
   testMatch: [
-    "<rootDir>/src/**/__tests__/**/*.[jt]s?(x)",
-    "<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)",
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
   ],
 };
