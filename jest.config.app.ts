b6144b5820a3ad05b517534a9a812a71c699a404
Dennis James Stelmach <dj@dstelmach.com>
2023-05-03 00:29:25 -0400

    Resolve code coverage errors.

--- jest.config.app.ts
+++ jest.config.app.ts
@@ -6,7 +6,6 @@ export default {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
   moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "./coverage/nx-pipenv",
   testMatch: [
     "<rootDir>/src/**/__tests__/**/*.[jt]s?(x)",
     "<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)",
