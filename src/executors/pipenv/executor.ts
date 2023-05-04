8d7d87a52f61cf94cfd0e2fc9cc4fafbc0e757e5
Dennis James Stelmach <dj@dstelmach.com>
2023-05-04 00:06:12 -0400

    Create and test pipenv executor.

---
+++ src/executors/pipenv/executor.ts
@@ -0,0 +1,23 @@
+import { ExecutorContext } from '@nx/devkit';
+import { PipenvExecutorSchema } from './schema';
+import { pipenv } from '../../pipenv/pipenv';
+
+export default async function runPipenv(
+  options: PipenvExecutorSchema,
+  context: ExecutorContext
+) {
+  const projectRoot =
+    context.projectsConfigurations.projects[context.projectName].root;
+  const { command, cwd, raw } = options;
+
+  return pipenv(command, cwd || projectRoot, raw)
+    .then(() => ({
+      success: true,
+    }))
+    .catch((error) => {
+      console.error(error);
+      return {
+        success: false,
+      };
+    });
+}
---
+++ src/executors/pipenv/schema.d.ts
@@ -0,0 +1,8 @@
+export interface PipenvExecutorSchema {
+  // The command to run. 'pipenv ' is prepended to this command.
+  command: string;
+  // Override where the command runs. By default, the command runs in the project root.
+  cwd?: string;
+  // Do not prepend 'pipenv ' to the given command.
+  raw: boolean;
+}
