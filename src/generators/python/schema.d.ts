1503106710a022981a508659e9b31274c5880107
Dennis James Stelmach <dj@dstelmach.com>
2023-05-27 00:54:13 -0400

    Add docs to python generator schema.

--- src/generators/python/schema.d.ts
+++ src/generators/python/schema.d.ts
@@ -3,9 +3,14 @@ import type { ProjectType } from '@nx/devkit';
 type BuildBackend = 'pdm-backend' | 'setuptools' | 'flot' | 'hatchling';
 
 export interface PythonGeneratorSchema {
+  // Name of the project.
   name: string;
+  // Application or Library.
   projectType: ProjectType;
+  // Override the default build backend.
   buildBackend?: BuildBackend;
+  // A diretory where the project is placed.
   directory?: string;
+  // Add tags to the project (used for linting).
   tags?: string;
 }
