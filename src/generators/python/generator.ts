6636ed6de1dfabf0cb5374f68ef3f74d9d7b92ce
Dennis James Stelmach <dj@dstelmach.com>
2023-06-06 18:55:04 -0400

    Add options to init E2E. Use end tasks array.

--- src/generators/python/generator.ts
+++ src/generators/python/generator.ts
@@ -9,7 +9,11 @@ import {
   joinPathFragments,
   ProjectType,
   TargetConfiguration,
+  ensurePackage,
+  NX_VERSION,
+  runTasksInSerial,
 } from '@nx/devkit';
+import { Linter as nxLinter } from '@nx/linter';
 import { readFileSync, writeFileSync, rmSync } from 'fs';
 import { BuildBackend, PythonGeneratorSchema } from './schema';
 import { dummyFiles } from './dummyFiles';
@@ -22,6 +26,8 @@ interface NormalizedOptions extends PythonGeneratorSchema {
   rootOffset: string;
   parsedTags: string[];
 }
+
+type EndTask = () => void | Promise<void>;
 
 const normalizeOptions = (
   tree: Tree,
@@ -58,7 +64,7 @@ const getTargets = ({
   typeChecker,
 }: NormalizedOptions) => {
   const executor = 'nx-python-pdm:pdm';
-  let testCommand;
+  let testCommand: string;
   if (unitTestRunner === 'unittest') {
     testCommand = 'run unittest discover .';
   } else if (unitTestRunner === 'pytest') {
@@ -82,7 +88,7 @@ const getTargets = ({
   };
 
   if (linter && linter !== 'none') {
-    let lintCommand;
+    let lintCommand: string;
     if (linter === 'pylint') {
       lintCommand = 'run pylint ./**/*.py';
     } else if (linter === 'flake8') {
@@ -103,7 +109,7 @@ const getTargets = ({
   }
 
   if (typeChecker && typeChecker !== 'none') {
-    let typeCheckCommand;
+    let typeCheckCommand: string;
     if (typeChecker === 'mypy') {
       typeCheckCommand = 'run mypy ./**/*.py';
     } else if (typeChecker === 'pyright') {
@@ -144,6 +150,7 @@ export const pdmInstallCommand = ({
   linter,
   typeChecker,
   unitTestRunner,
+  e2eTestRunner,
 }: NormalizedOptions) => {
   let pdmInstallCommand = `add -d`;
   if (unitTestRunner !== 'unittest') {
@@ -165,16 +172,48 @@ export const pdmInstallCommand = ({
   ) {
     pdmInstallCommand += ` ${typeChecker}`;
   }
+  // includes is not strictly neccessary. Sanity check
+  if (
+    e2eTestRunner === 'robot' &&
+    !pdmInstallCommand.includes(' robotframework')
+  ) {
+    pdmInstallCommand += 'robotframework';
+  }
   return pdmInstallCommand;
+};
+
+// Only Cypress calls this function for now
+// as all others do not require any extra tasks.
+const addE2E = async (
+  tree: Tree,
+  { projectName }: NormalizedOptions
+): Promise<EndTask> => {
+  const { cypressE2EConfigurationGenerator } = ensurePackage<
+    typeof import('@nx/cypress')
+  >('@nx/cypress', NX_VERSION);
+
+  return await cypressE2EConfigurationGenerator(tree, {
+    project: projectName,
+    linter: nxLinter.EsLint,
+  });
 };
 
 export async function pythonGenerator(
   tree: Tree,
   options: PythonGeneratorSchema
 ) {
+  const endTasks: EndTask[] = [];
   const normalizedOptions = normalizeOptions(tree, options);
-  const { buildBackend, parsedTags, projectName, projectRoot, projectType } =
-    normalizedOptions;
+  const {
+    buildBackend,
+    e2eTestRunner,
+    parsedTags,
+    projectName,
+    projectRoot,
+    projectType,
+  } = normalizedOptions;
+
+  // Add main project
   addProjectConfiguration(tree, projectName, {
     root: projectRoot,
     projectType: projectType,
@@ -197,37 +236,46 @@ export async function pythonGenerator(
   tree.write(joinPathFragments(projectRoot, '.pdm-python'), '');
   tree.write(joinPathFragments(projectRoot, '.gitignore'), '');
 
+  if (e2eTestRunner === 'cypress') {
+    endTasks.push(await addE2E(tree, normalizedOptions));
+  }
+
   await formatFiles(tree);
 
   return async () => {
-    const cwd = joinPathFragments(tree.root, projectRoot);
-    dummyFiles.forEach((dummyFile) => {
-      rmSync(joinPathFragments(cwd, dummyFile));
-    });
+    // Initialize PDM specifics
+    endTasks.unshift(async () => {
+      const cwd = joinPathFragments(tree.root, projectRoot);
+      dummyFiles.forEach((dummyFile) => {
+        rmSync(joinPathFragments(cwd, dummyFile));
+      });
 
-    await pdm(pdmInitCommand(projectType, buildBackend), {
-      cwd,
-    });
+      await pdm(pdmInitCommand(projectType, buildBackend), {
+        cwd,
+      });
 
-    // Add project name, version, and authors as the minimum needed to build
-    // PDM automatically gives project name and version for libraries, but applications do not for some reason
-    const tomlPath = joinPathFragments(cwd, 'pyproject.toml');
-    const pyprojectContent = readFileSync(tomlPath)
-      .toString()
-      // Add the project name
-      .replace(/(^name\s*=\s*)("")/gm, `$1"${projectName}"`)
-      // Add the version
-      .replace(/(^version\s*=\s*)("")/gm, '$1"0.1.0"')
-      // Add the authors
-      .replace(
-        /(^authors\s*=\s*)(\[\s*\{name\s*=\s*"", email\s*=\s*""\},\s*\])/gm,
-        '$1[\n    {name = "Your Name", email = "your@email.com"},\n]'
-      );
-    writeFileSync(tomlPath, pyprojectContent);
+      // Add project name, version, and authors as the minimum needed to build
+      // PDM automatically gives project name and version for libraries, but applications do not for some reason
+      const tomlPath = joinPathFragments(cwd, 'pyproject.toml');
+      const pyprojectContent = readFileSync(tomlPath)
+        .toString()
+        // Add the project name
+        .replace(/(^name\s*=\s*)("")/gm, `$1"${projectName}"`)
+        // Add the version if not present
+        .replace(/(^version\s*=\s*)("")/gm, '$1"0.1.0"')
+        // Add boilerplate to the authors list
+        .replace(
+          /(^authors\s*=\s*)(\[\s*\{name\s*=\s*"", email\s*=\s*""\},\s*\])/gm,
+          '$1[\n    {name = "Your Name", email = "your@email.com"},\n]'
+        );
+      writeFileSync(tomlPath, pyprojectContent);
 
-    await pdm(pdmInstallCommand(normalizedOptions), {
-      cwd,
+      await pdm(pdmInstallCommand(normalizedOptions), {
+        cwd,
+      });
     });
+    
+    runTasksInSerial(...endTasks);
   };
 }
 
