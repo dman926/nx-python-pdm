bec50fffaa7584932b757f22c4934975b3f8a0fe
Dennis James Stelmach <dj@dstelmach.com>
2023-08-25 15:23:32 -0400

    Use executor option factory instead of redefining.

--- src/executors/pdm/executor.spec.ts
+++ src/executors/pdm/executor.spec.ts
@@ -10,9 +10,10 @@ jest.mock('../../pdm/pdm', () => ({
 
 const mockpdm = jest.mocked(pdm);
 
-const defaultOptions: Partial<pdmExecutorSchema> = {
+const getOptions = (options: pdmExecutorSchema): pdmExecutorSchema => ({
+  ...options,
   quiet: true,
-};
+});
 
 const mockContext: ExecutorContext = {
   root: '',
@@ -35,10 +36,9 @@ describe('pdm Executor', () => {
   });
 
   it('should call pdm with the command and project root', async () => {
-    const options: pdmExecutorSchema = {
-      ...defaultOptions,
+    const options = getOptions({
       command: 'install',
-    };
+    });
     await runpdm(options, mockContext);
     expect(mockpdm).toHaveBeenCalledWith('install', {
       cwd: '/path/to/project',
@@ -46,11 +46,10 @@ describe('pdm Executor', () => {
   });
 
   it('should call pdm with the provided cwd if specified', async () => {
-    const options: pdmExecutorSchema = {
-      ...defaultOptions,
+    const options = getOptions({
       command: 'run',
       cwd: '/path/to/cwd',
-    };
+    });
     await runpdm(options, mockContext);
     expect(mockpdm).toHaveBeenCalledWith('run', {
       cwd: '/path/to/cwd',
@@ -58,11 +57,10 @@ describe('pdm Executor', () => {
   });
 
   it('should call pdm with the "raw" option if specified', async () => {
-    const options: pdmExecutorSchema = {
-      ...defaultOptions,
+    const options = getOptions({
       command: 'install',
       raw: true,
-    };
+    });
     await runpdm(options, mockContext);
     expect(mockpdm).toHaveBeenCalledWith('install', {
       cwd: '/path/to/project',
@@ -72,10 +70,9 @@ describe('pdm Executor', () => {
 
   it('should return an object with "success" set to true if pdm succeeds', async () => {
     mockpdm.mockResolvedValueOnce({ stdout: 'pdm output', stderr: '' });
-    const options: pdmExecutorSchema = {
-      ...defaultOptions,
+    const options = getOptions({
       command: 'install',
-    };
+    });
     const result = await runpdm(options, mockContext);
     expect(result.success).toBe(true);
   });
@@ -86,10 +83,9 @@ describe('pdm Executor', () => {
 
     const error = new Error('pdm error');
     mockpdm.mockRejectedValueOnce(error);
-    const options: pdmExecutorSchema = {
-      ...defaultOptions,
+    const options = getOptions({
       command: 'install',
-    };
+    });
     const result = await runpdm(options, mockContext);
     expect(result.success).toBe(false);
     expect(mockConsoleError).toHaveBeenCalledWith(error);
