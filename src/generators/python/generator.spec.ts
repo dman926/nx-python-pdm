9c00112845fcc8caff4e1e9aed4ea41a58408ddd
Dennis James Stelmach <dj@dstelmach.com>
2023-09-15 22:19:25 -0400

    Unit test projectType.

--- src/generators/python/generator.spec.ts
+++ src/generators/python/generator.spec.ts
@@ -3,6 +3,7 @@ import {
   type Tree,
   joinPathFragments,
   readProjectConfiguration,
+  ProjectType,
 } from '@nx/devkit';
 import { writeFile } from 'fs/promises';
 
@@ -120,99 +121,111 @@ const unitTestRunners: { name: UnitTestRunner; command?: string }[] = [
 //   { name: 'robot', command: '' },
 // ];
 
-describe('python generator', () => {
-  let tree: Tree;
-  const options: PythonGeneratorSchema = {
-    name: 'test',
-    projectType: 'application',
-  };
-  const expectedPyprojectToml = `[tool.pdm]\n\n[project]\nname = "${options.name}"\nversion = "0.1.0"\ndescription = ""\nauthors = [\n    {name = "Your Name", email = "your@email.com"},\n]\n`;
-  const cwd = '/virtual/test';
+const projectTypes: ProjectType[] = ['library', 'application'];
 
-  beforeEach(() => {
-    tree = createTreeWithEmptyWorkspace();
-  });
+projectTypes.forEach((projectType) => {
+  describe(`python generator - ${projectType}`, () => {
+    let tree: Tree;
+    const options: PythonGeneratorSchema = {
+      name: 'test',
+      projectType,
+    };
+    const expectedPyprojectToml = `[tool.pdm]\n\n[project]\nname = "${options.name}"\nversion = "0.1.0"\ndescription = ""\nauthors = [\n    {name = "Your Name", email = "your@email.com"},\n]\n`;
+    const cwd = '/virtual/test';
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('should run successfully', async () => {
-    await pythonGenerator(tree, options);
-    const config = readProjectConfiguration(tree, 'test');
-    expect(config).toBeDefined();
-  });
-
-  it('should return a function that configures pdm', async () => {
-    const outputFn = await pythonGenerator(tree, options);
-    await outputFn();
-    // Inits PDM
-    expect(mockPdm).toBeCalledWith(pdmInitCommand(options.projectType), {
-      cwd,
+    beforeEach(() => {
+      tree = createTreeWithEmptyWorkspace();
     });
-    // Updates project name and version
-    expect(mockWriteFile).toBeCalledWith(
-      joinPathFragments(cwd, 'pyproject.toml'),
-      expectedPyprojectToml
-    );
-  });
 
-  it('should add a target for test with the correctly specified unit test runner', async () => {
-    for (const runner of unitTestRunners) {
-      tree = createTreeWithEmptyWorkspace();
-      const optionsWithAdditionalTarget: PythonGeneratorSchema = {
-        ...options,
-        unitTestRunner: runner.name,
-      };
-      await pythonGenerator(tree, optionsWithAdditionalTarget);
-      const config = readProjectConfiguration(tree, 'test');
-      expect(config.targets?.test).toBeDefined();
-      expect(config.targets?.test.options.command).toContain(runner.command);
-    }
-  });
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
 
-  it('should add a target for lint with the correctly specified linter', async () => {
-    for (const linter of linters) {
-      tree = createTreeWithEmptyWorkspace();
-      const optionsWithAdditionalTarget: PythonGeneratorSchema = {
-        ...options,
-        linter: linter.name,
-      };
-      await pythonGenerator(tree, optionsWithAdditionalTarget);
+    it('should run successfully', async () => {
+      await pythonGenerator(tree, options);
       const config = readProjectConfiguration(tree, 'test');
-      if (linter.name !== 'none') {
-        expect(config.targets?.lint).toBeDefined();
-        expect(config.targets?.lint.options.command).toContain(linter.command);
-      } else {
-        // When 'none' is specified, no lint target is added
-        expect(config.targets?.lint).not.toBeDefined();
+      expect(config).toBeDefined();
+    });
+
+    it('should return a function that configures pdm', async () => {
+      const outputFn = await pythonGenerator(tree, options);
+      await outputFn();
+      // Inits PDM
+      expect(mockPdm).toBeCalledWith(pdmInitCommand(options.projectType), {
+        cwd,
+      });
+      // Updates project name and version
+      expect(mockWriteFile).toBeCalledWith(
+        joinPathFragments(cwd, 'pyproject.toml'),
+        expectedPyprojectToml
+      );
+    });
+
+    it('should correctly configure the projectType', async () => {
+      await pythonGenerator(tree, options);
+      const config = readProjectConfiguration(tree, 'test');
+      expect(config.projectType).toBe(options.projectType);
+    });
+
+    it('should correctly add a target for test with the specified unit test runner', async () => {
+      for (const runner of unitTestRunners) {
+        tree = createTreeWithEmptyWorkspace();
+        const optionsWithAdditionalTarget: PythonGeneratorSchema = {
+          ...options,
+          unitTestRunner: runner.name,
+        };
+        await pythonGenerator(tree, optionsWithAdditionalTarget);
+        const config = readProjectConfiguration(tree, 'test');
+        expect(config.targets?.test).toBeDefined();
+        expect(config.targets?.test.options.command).toContain(runner.command);
       }
-    }
-  });
+    });
 
-  it('should add a target for type checking with the correctly specified type check runner', async () => {
-    for (const runner of typeCheckers) {
-      tree = createTreeWithEmptyWorkspace();
-      const optionsWithAdditionalTarget: PythonGeneratorSchema = {
-        ...options,
-        typeChecker: runner.name,
-      };
-      await pythonGenerator(tree, optionsWithAdditionalTarget);
-      const config = readProjectConfiguration(tree, 'test');
-      if (runner.name !== 'none') {
-        expect(config.targets?.typeCheck).toBeDefined();
-        expect(config.targets?.typeCheck.options.command).toContain(
-          runner.command
-        );
-      } else {
-        // When 'none' is specified, no typeCheck target is added
-        expect(config.targets?.typeCheck).not.toBeDefined();
+    it('should correctly add a target for lint with the specified linter', async () => {
+      for (const linter of linters) {
+        tree = createTreeWithEmptyWorkspace();
+        const optionsWithAdditionalTarget: PythonGeneratorSchema = {
+          ...options,
+          linter: linter.name,
+        };
+        await pythonGenerator(tree, optionsWithAdditionalTarget);
+        const config = readProjectConfiguration(tree, 'test');
+        if (linter.name !== 'none') {
+          expect(config.targets?.lint).toBeDefined();
+          expect(config.targets?.lint.options.command).toContain(
+            linter.command
+          );
+        } else {
+          // When 'none' is specified, no lint target is added
+          expect(config.targets?.lint).not.toBeDefined();
+        }
       }
-    }
-  });
+    });
 
-  // Skipping for now to merge to main. They technically get added. But they need to be tested.
-  it.skip('should add E2E configurations properly', () => {
-    expect('E2E TESTS NOT ADDED!').toBeFalsy();
+    it('should correctly add a target for type checking with the specified type check runner', async () => {
+      for (const runner of typeCheckers) {
+        tree = createTreeWithEmptyWorkspace();
+        const optionsWithAdditionalTarget: PythonGeneratorSchema = {
+          ...options,
+          typeChecker: runner.name,
+        };
+        await pythonGenerator(tree, optionsWithAdditionalTarget);
+        const config = readProjectConfiguration(tree, 'test');
+        if (runner.name !== 'none') {
+          expect(config.targets?.typeCheck).toBeDefined();
+          expect(config.targets?.typeCheck.options.command).toContain(
+            runner.command
+          );
+        } else {
+          // When 'none' is specified, no typeCheck target is added
+          expect(config.targets?.typeCheck).not.toBeDefined();
+        }
+      }
+    });
+
+    // Skipping for now to merge to main. They technically get added. But they need to be tested.
+    it.skip('TODO: should correctly add E2E configurations with the specified E2E runner', () => {
+      expect('E2E TESTS NOT ADDED!').toBeFalsy();
+    });
   });
 });
